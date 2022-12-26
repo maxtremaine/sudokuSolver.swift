8af5d64cb0f58ab156b6a42f037a8d78c83be3d5
Max Tremaine <max.tremaine@gmail.com>
2022-12-25 23:45:41 -0500

    Initialized Swift.

---
+++ Sources/sudokuSolver.swift/sudokuSolver_swift.swift
@@ -0,0 +1,8 @@
+@main
+public struct sudokuSolver_swift {
+    public private(set) var text = "Hello, World!"
+
+    public static func main() {
+        print(sudokuSolver_swift().text)
+    }
+}
---
+++ Tests/sudokuSolver.swiftTests/sudokuSolver_swiftTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import sudokuSolver_swift
+
+final class sudokuSolver_swiftTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(sudokuSolver_swift().text, "Hello, World!")
+    }
+}
