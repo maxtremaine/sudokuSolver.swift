c74753ff25f87266e4b2c6b837671d169486fa76
maxtremaine <max.tremaine@gmail.com>
2022-12-27 12:59:08 -0500

    Removed string functionality in favour of a list of integers.

--- Sources/sudokuSolver/Sudoku.swift
+++ Sources/sudokuSolver/Sudoku.swift
@@ -1,18 +1,5 @@
 public struct Sudoku {
     let values: [Int]
-    
-    // If a string is 81 digits or underscores, return true.
-    public static func isSudokuString(_ sudokuString: String) -> Bool {
-        if (sudokuString.count != 81) { return false }
-
-        for character in sudokuString {
-            if (!sudokuStringValues.contains(String(character))) {
-                return false
-            }
-        }
-        
-        return true
-    }
 
     public static func isSudokuFile(_ sudokuFile: String) -> Bool {
         if (sudokuFile.count != 167) { return false }
@@ -25,12 +12,14 @@ public struct Sudoku {
 
         return true
     }
+
+    public func isValid() -> Bool {
+        return true
+    }
 }
 
-private let sudokuStringValues = [ "_", "1", "2", "3", "4", "5", "6", "7", "8", "9" ]
-
-private let sudokuFileValues = [ " ", "\n", "|", "_", "-", "a", "b", "c", "d", "e", "f", "g",
-    "h", "i"] + sudokuStringValues
+private let sudokuFileValues = [ "_", "1", "2", "3", "4", "5", "6", "7", "8", "9", " ", "\n", "|", "_", "-", "a", "b", "c", "d", "e", "f", "g",
+    "h", "i"]
 
 
 
@@ -39,3 +28,36 @@ private let fileToStringConversionIndexes = [ 16, 17, 18, 20, 21, 22, 24, 25, 26
     86, 87, 88, 90, 91, 92, 94, 95, 96, 100, 101, 102, 104, 105, 106, 109, 110, 128, 129, 130,
     132, 133, 134, 136, 137, 138, 142, 143, 144, 146, 147, 148, 150, 151, 152, 156, 157, 158,
     160, 161, 162, 164, 165, 166 ]
+
+private let groups = [
+    // Rows
+    [ 0, 1, 2, 3, 4, 5, 6, 7,  8 ],
+    [ 9, 10, 11, 12, 13, 14, 15, 16, 17 ],
+    [ 18, 19, 20, 21, 22, 23, 24, 25, 26 ],
+    [ 27, 28, 29, 30, 31, 32, 33, 34, 35 ],
+    [ 36, 37, 38, 39, 40, 41, 42, 43, 44 ],
+    [ 45, 46, 47, 48, 49, 50, 51, 52, 53 ],
+    [ 54, 55, 56, 57, 58, 59, 60, 61, 62 ],
+    [ 63, 64, 65, 66, 67, 68, 69, 70, 71 ],
+    [ 72, 73, 74, 75, 76, 77, 78, 79, 80 ],
+    // Columns
+    [ 0, 9, 18, 27, 36, 45, 54, 63, 72 ],
+    [ 1, 10, 19, 28, 37, 46, 55, 64, 73 ],
+    [ 2, 11, 20, 29, 38, 47, 56, 65, 74 ],
+    [ 3, 12, 21, 30, 39, 48, 57, 66, 75 ],
+    [ 4, 13, 22, 31, 40, 49, 58, 67, 76 ],
+    [ 5, 14, 23, 32, 41, 50, 59, 68, 77 ],
+    [ 6, 15, 24, 33, 42, 51, 60, 69, 78 ],
+    [ 7, 16, 25, 34, 43, 52, 61, 70, 79 ],
+    [ 8, 17, 26, 35, 44, 53, 62, 71, 80 ],
+    // Boxes
+    [ 0, 1, 2, 9, 10, 11, 18, 19, 20 ],
+    [ 3, 4, 5, 12, 13, 14, 21, 22, 23 ],
+    [ 6, 7, 8, 15, 16, 17, 24, 25, 26 ],
+    [ 27, 28, 29, 36, 37, 38, 45, 46, 47 ],
+    [ 30, 31, 32, 39, 40, 41, 48, 49, 50 ],
+    [ 33, 34, 35, 42, 43, 44, 51, 52, 53 ],
+    [ 54, 55, 56, 63, 64, 65, 72, 73, 74 ],
+    [ 57, 58, 59, 66, 67, 68, 75, 76, 77 ],
+    [ 60, 61, 62, 69, 70, 71, 78, 79, 80 ]
+]
--- Tests/sudokuSolverTests/SudokuTests.swift
+++ Tests/sudokuSolverTests/SudokuTests.swift
@@ -2,18 +2,6 @@ import XCTest
 @testable import sudokuSolver
 
 final class SudokuTests: XCTestCase {
-    func testIsSudokuString() throws {
-        let validSudokuString = "7___4___1__1___2___6_2_9_8___35_49__1_______4__21_85___1_9_6_7___8___4__6___2___8"
-        let badCharacter = "7a__4___1__1___2___6_2_9_8___35_49__1_______4__21_85___1_9_6_7___8___4__6___2___8"
-        let long = "7___4___1__1___2___6_2_9_8___35_49__1_______4__21_85___1_9_6_7___8___4__6___2___8_"
-        let short = "7___4___1__1___2___6_2_9_8___35_49__1_______4__21_85___1_9_6_7___8___4__6___2___"
-
-        XCTAssertTrue(Sudoku.isSudokuString(validSudokuString))
-        XCTAssertFalse(Sudoku.isSudokuString(badCharacter))
-        XCTAssertFalse(Sudoku.isSudokuString(long))
-        XCTAssertFalse(Sudoku.isSudokuString(short))
-    }
-
     func testIsSudokuFile() throws {
         let validFile = [
             "  abc def ghi",
