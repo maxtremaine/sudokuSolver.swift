5320b5176d4107fd158bcff6044d64bbbaefb506
maxtremaine <max.tremaine@gmail.com>
2022-12-27 12:30:11 -0500

    Validates .sudoku files and working strings.

--- Sources/sudokuSolver/Sudoku.swift
+++ Sources/sudokuSolver/Sudoku.swift
@@ -3,21 +3,36 @@ public struct Sudoku {
     
     // If a string is 81 digits or underscores, return true.
     public static func isSudokuString(_ sudokuString: String) -> Bool {
-        if (sudokuString.count != 81) {
-            return false
-        }
+        if (sudokuString.count != 81) { return false }
 
-        let correctCharacters = sudokuString.allSatisfy {
-            $0 == "_" || $0 == "1" || $0 == "2" || $0 == "3" || $0 == "4" || $0 == "5" || $0 == "6"
-            || $0 == "7" || $0 == "8" || $0 == "9"
-        }
-        if (!correctCharacters) {
-            return false
+        for character in sudokuString {
+            if (!sudokuStringValues.contains(String(character))) {
+                return false
+            }
         }
         
         return true
     }
+
+    public static func isSudokuFile(_ sudokuFile: String) -> Bool {
+        if (sudokuFile.count != 167) { return false }
+
+        for character in sudokuFile {
+            if (!sudokuFileValues.contains(String(character))) {
+                return false
+            }
+        }
+
+        return true
+    }
 }
+
+private let sudokuStringValues = [ "_", "1", "2", "3", "4", "5", "6", "7", "8", "9" ]
+
+private let sudokuFileValues = [ " ", "\n", "|", "_", "-", "a", "b", "c", "d", "e", "f", "g",
+    "h", "i"] + sudokuStringValues
+
+
 
 private let fileToStringConversionIndexes = [ 16, 17, 18, 20, 21, 22, 24, 25, 26, 30, 31, 32, 34,
     35, 36, 38, 39, 40, 44, 45, 46, 48, 49, 50, 52, 53, 54, 72, 73, 74, 76, 77, 78, 80, 81, 82,
--- Tests/sudokuSolverTests/SudokuTests.swift
+++ Tests/sudokuSolverTests/SudokuTests.swift
@@ -13,4 +13,35 @@ final class SudokuTests: XCTestCase {
         XCTAssertFalse(Sudoku.isSudokuString(long))
         XCTAssertFalse(Sudoku.isSudokuString(short))
     }
+
+    func testIsSudokuFile() throws {
+        let validFile = [
+            "  abc def ghi",
+            "1 7__|_4_|__1",
+            "2 __1|___|2__",
+            "3 _6_|2_9|_8_",
+            "  -----------",
+            "4 __3|5_4|9__",
+            "5 1__|___|__4",
+            "6 __2|1_8|5__",
+            "  -----------",
+            "7 _1_|9_6|_7_",
+            "8 __8|___|4__",
+            "9 6__|_2_|__8"].joined(separator: "\n")
+        let badCharacter = [
+            "  abc def ghi",
+            "1 7__|_4_|__1",
+            "2 __1|_w_|2__",
+            "3 _6_|2_9|_8_",
+            "  -----------",
+            "4 __3|5_4|9__",
+            "5 1__|___|__4",
+            "6 __2|1_8|5__",
+            "  -----------",
+            "7 _1_|9_6|_7_",
+            "8 __8|___|4__",
+            "9 6__|_2_|__8"].joined(separator: "\n")
+        XCTAssertTrue(Sudoku.isSudokuFile(validFile))
+        XCTAssertFalse(Sudoku.isSudokuFile(badCharacter))
+    }
 }
