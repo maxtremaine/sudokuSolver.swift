a606102b2ffaa612c000fa4491873fdf2fdb50fd
maxtremaine <max.tremaine@gmail.com>
2022-12-27 22:09:11 -0500

    Asserts sudoku file.

--- Sources/sudokuSolver/main.swift
+++ Sources/sudokuSolver/main.swift
@@ -4,4 +4,6 @@ let sudokuFile = try String(contentsOfFile: "./IO/start.sudoku")
 
 let t0 = Date()
 
+assert(Sudoku.isSudokuFile(sudokuFile), "Sudoku file is not valid.")
+
 print("Ran in \(Date().timeIntervalSince(t0)) seconds.")
