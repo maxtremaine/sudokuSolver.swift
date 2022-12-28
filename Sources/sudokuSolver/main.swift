4af9dcb23bd2e676521916cbb1ae93a826f86d0d
maxtremaine <max.tremaine@gmail.com>
2022-12-28 16:02:19 -0500

    Solves puzzles.

--- Sources/sudokuSolver/main.swift
+++ Sources/sudokuSolver/main.swift
@@ -10,4 +10,27 @@ let startPuzzle = Sudoku.fromSudokuFile(sudokuFile)
 
 assert(startPuzzle.isValid(), "Values in io/start.sudoku do not form a valid Sudoku puzzle.")
 
+let numberOfRuns = startPuzzle.cells.filter { $0 == 0 }.count
+
+let endPuzzles: [Sudoku] = Array(1...numberOfRuns).reduce(into: [ startPuzzle ]) {
+    workingBranches, runNumber in
+
+    let newWorkingBranches: [Sudoku] = workingBranches.reduce(into: []) {
+        newBranches, oldBranch in
+
+        let blankCells = oldBranch.getBlankCells()
+            .sorted { $0.possibleValues.count < $1.possibleValues.count }
+        
+        for possibleValue in blankCells[0].possibleValues {
+            newBranches.append(Sudoku(oldBranch.cells.replace(blankCells[0].index, newValue: possibleValue)))
+        }
+    }
+
+    print("Completed run \(runNumber) with \(newWorkingBranches.count) branches.")
+
+    workingBranches = newWorkingBranches
+}
+
+print(endPuzzles)
+
 print("Ran in \(Date().timeIntervalSince(t0)) seconds.")
