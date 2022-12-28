11532d708dc32a33cd026e9c179155205ffd15d2
maxtremaine <max.tremaine@gmail.com>
2022-12-27 22:39:04 -0500

    Tests fromSudokuFile.

--- Tests/sudokuSolverTests/SudokuTests.swift
+++ Tests/sudokuSolverTests/SudokuTests.swift
@@ -45,4 +45,26 @@ final class SudokuTests: XCTestCase {
         XCTAssertTrue(Sudoku(valid_puzzle).isValid())
         XCTAssertFalse(Sudoku(invalid_puzzle).isValid())
     }
+
+    func testFromSudokuFile() throws {
+        let sudokuFile = [
+            "  abc def ghi",
+            "1 7__|_4_|__1",
+            "2 __1|___|2__",
+            "3 _6_|2_9|_8_",
+            "  -----------",
+            "4 __3|5_4|9__",
+            "5 1__|___|__4",
+            "6 __2|1_8|5__",
+            "  -----------",
+            "7 _1_|9_6|_7_",
+            "8 __8|___|4__",
+            "9 6__|_2_|__8"].joined(separator: "\n")
+        let sudokuValues = [ 7, 0, 0, 0, 4, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 2, 0, 0, 0, 6, 0, 2, 0,
+            9, 0, 8, 0, 0, 0, 3, 5, 0, 4, 9, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 4, 0, 0, 2, 1, 0, 8,
+            5, 0, 0, 0, 1, 0, 9, 0, 6, 0, 7, 0, 0, 0, 8, 0, 0, 0, 4, 0, 0, 6, 0, 0, 0, 2, 0, 0,
+            0, 8]
+
+        XCTAssertEqual(Sudoku.fromSudokuFile(sudokuFile).values, sudokuValues)
+    }
 }
