78a626c73860b9f4b7901e2cf64109ca51b0a9f6
maxtremaine <max.tremaine@gmail.com>
2022-12-27 22:39:30 -0500

    Cleaned up string creation.

--- Tests/sudokuSolverTests/SudokuTests.swift
+++ Tests/sudokuSolverTests/SudokuTests.swift
@@ -15,7 +15,8 @@ final class SudokuTests: XCTestCase {
             "  -----------",
             "7 _1_|9_6|_7_",
             "8 __8|___|4__",
-            "9 6__|_2_|__8"].joined(separator: "\n")
+            "9 6__|_2_|__8"
+        ].joined(separator: "\n")
         let badCharacter = [
             "  abc def ghi",
             "1 7__|_4_|__1",
@@ -28,7 +29,8 @@ final class SudokuTests: XCTestCase {
             "  -----------",
             "7 _1_|9_6|_7_",
             "8 __8|___|4__",
-            "9 6__|_2_|__8"].joined(separator: "\n")
+            "9 6__|_2_|__8"
+        ].joined(separator: "\n")
         
         XCTAssertTrue(Sudoku.isSudokuFile(validFile))
         XCTAssertFalse(Sudoku.isSudokuFile(badCharacter))
@@ -59,7 +61,8 @@ final class SudokuTests: XCTestCase {
             "  -----------",
             "7 _1_|9_6|_7_",
             "8 __8|___|4__",
-            "9 6__|_2_|__8"].joined(separator: "\n")
+            "9 6__|_2_|__8"
+        ].joined(separator: "\n")
         let sudokuValues = [ 7, 0, 0, 0, 4, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 2, 0, 0, 0, 6, 0, 2, 0,
             9, 0, 8, 0, 0, 0, 3, 5, 0, 4, 9, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 4, 0, 0, 2, 1, 0, 8,
             5, 0, 0, 0, 1, 0, 9, 0, 6, 0, 7, 0, 0, 0, 8, 0, 0, 0, 4, 0, 0, 6, 0, 0, 0, 2, 0, 0,
