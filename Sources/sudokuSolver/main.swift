9d093b99b0bdea3c22f99cf2821957bc21df2519
maxtremaine <max.tremaine@gmail.com>
2022-12-27 22:40:42 -0500

    Added a starting point.

--- Sources/sudokuSolver/main.swift
+++ Sources/sudokuSolver/main.swift
@@ -6,6 +6,6 @@ let t0 = Date()
 
 assert(Sudoku.isSudokuFile(sudokuFile), "Sudoku file is not valid.")
 
-print(Sudoku.fromSudokuFile(sudokuFile))
+let startPuzzle = Sudoku.fromSudokuFile(sudokuFile)
 
 print("Ran in \(Date().timeIntervalSince(t0)) seconds.")
