f9f317f70a2b67bc7ba0d6b5a08df78b9334f808
maxtremaine <max.tremaine@gmail.com>
2022-12-29 10:44:04 -0500

    Added comments.

--- Sources/sudokuSolver/main.swift
+++ Sources/sudokuSolver/main.swift
@@ -12,16 +12,19 @@ assert(startPuzzle.isValid(), "Values in io/start.sudoku do not form a valid Sud
 
 let numberOfRuns = startPuzzle.cells.filter { $0 == 0 }.count
 
+// Iterate through the number of blank cells, adding possible values until branches collapse.
 let endPuzzles: [Sudoku] = Array(1...numberOfRuns).reduce(into: [ startPuzzle ]) {
     workingBranches, runNumber in
 
     let newWorkingBranches: [Sudoku] = workingBranches.reduce(into: []) {
         newBranches, oldBranch in
 
+        // Sort the blank cells by degrees of freedom (possibleValues).
         let blankCells = oldBranch.getBlankCells()
             .sorted { $0.possibleValues.count < $1.possibleValues.count }
         
         for possibleValue in blankCells[0].possibleValues {
+            // Add a new branch with a possible value in place of the blank cell.
             newBranches.append(Sudoku(oldBranch.cells.replace(blankCells[0].index, newValue: possibleValue)))
         }
     }
